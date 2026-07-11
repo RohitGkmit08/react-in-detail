7d767b6aed01ce3d260c40aeeed531e3266e0d94
Rohit Sinha <rohitsinha0812@gmail.com>
2026-07-11 16:59:59 +0530

    feat: wrap EmployeeCard with React.memo

--- project/src/components/EmployeeCard.jsx
+++ project/src/components/EmployeeCard.jsx
@@ -1,15 +1,20 @@
-const EmployeeCard = ({employee, onDelete}) => {
-   console.log(`${employee.name} Card Rendered`);
-  return (
-    <div className="employee-card">
-      <p className="employee-card-text">Name: {employee.name}</p>
-      <p className="employee-card-text">Email: {employee.email}</p>
-      <p className="employee-card-text">Department: {employee.department}</p>
-      <button onClick={() => onDelete(employee.id)}>
-        Delete
-      </button>
-    </div>
-  )
-}
+import React from "react";
+
+const EmployeeCard = React.memo(({ employee, onDelete }) => {
+
+    console.log(`${employee.name} Card Rendered`);
+
+    return (
+        <div className="employee-card">
+            <p>Name: {employee.name}</p>
+            <p>Email: {employee.email}</p>
+            <p>Department: {employee.department}</p>
+
+            <button onClick={() => onDelete(employee.id)}>
+                Delete
+            </button>
+        </div>
+    );
+});
 
 export default EmployeeCard;
